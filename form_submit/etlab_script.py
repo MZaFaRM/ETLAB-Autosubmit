4c303811b8a8fee1a11467136cffbdc1fd0c815e
Muhammed Zafar <muhammedzafar.mm@gmail.com>
2024-01-17 16:49:04 +0530

    [MINOR] Some minor changes

--- form_submit/etlab_script.py
+++ form_submit/etlab_script.py
@@ -14,12 +14,6 @@ class ETLab:
         self.username = username
         self.password = password
         self.session = requests.Session()
-        self.session.headers.update(
-            {
-                "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 \
-                (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36"
-            }
-        )
         self.login()
 
     def login(self):
